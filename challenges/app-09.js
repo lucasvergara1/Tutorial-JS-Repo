16b990a7e70cee5a92571d14b71829e49e050537
Lucas <lucas.vergara@live.com>
2024-03-02 22:46:58 -0300

    update

---
+++ challenges/app-09.js
@@ -0,0 +1,12 @@
+const gas = [2, 3, 4, 30]
+const food = [10,40,50]
+
+function calculateTotal(arr){
+let total = 0;
+
+forw (let i=0; i < arr.length; i++){
+console.log(arr[1]);
+};
+}
+
+calculateTotal(gas);
