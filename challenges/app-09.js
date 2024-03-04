14b6c6189a547f9a9c419f528a68b1dd771a80e6
Lucas <lucas.vergara@live.com>
2024-03-04 19:29:40 -0300

    update

--- challenges/app-09.js
+++ challenges/app-09.js
@@ -1,12 +1,8 @@
-const gas = [2, 3, 4, 30]
-const food = [10,40,50]
 
-function calculateTotal(arr){
-let total = 0;
+const updatedStudents = students.map(function(orange){
+    //console.log(orange.name);
+    orange.role = 'student'
+    return orange;
+});
 
-forw (let i=0; i < arr.length; i++){
-console.log(arr[1]);
-};
-}
-
-calculateTotal(gas);
+console.log(updatedStudents);
