8133cea01ca3f495378f1f7e4d2584e0b8f0a5b1
Lucas <lucas.vergara@live.com>
2024-03-04 19:47:09 -0300

    update

--- challenges/app-09.js
+++ challenges/app-09.js
@@ -5,4 +5,14 @@ const updatedStudents = students.map(function(orange){
     return orange;
 });
 
-console.log(updatedStudents);
+//console.log(updatedStudents);
+
+const highScores = students.filter(function(student) {
+   // if (student.score >= 80) { // opção 1 de solução, abaixo fizemos outra possibilidade com mesmo resultado
+    //    return student;
+    //}
+    //if (student.score >=80) return student; // aqui opção 2 de resolução
+    return student.score >= 80; // aqui opção de resolução 3
+});
+
+console.log(highScores);
